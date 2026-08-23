5670af7f55fc4a449921ee581e44538affb4cc6f
Axel-monix <anggatyan2@gmail.com>
2026-08-23 20:17:26 +0700

    feat(manage-admin): connect application routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./src/routes");
 const cors = require("cors");
+const adminRoutes = require("./src/routes/adminRoutes");
 
 const app = express();
 const PORT = 5000;
@@ -8,6 +9,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
+app.use("/api/admins", adminRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
